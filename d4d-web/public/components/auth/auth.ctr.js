fb497f526b6ca3d5cd808c7516c9d4dd48f11787
Lasith-Niro <lasithniro.online@gmail.com>
2017-05-23 16:19:33 +0530

    update login function

--- d4d-web/public/components/auth/auth.ctr.js
+++ d4d-web/public/components/auth/auth.ctr.js
@@ -1,5 +1,7 @@
 //https://github.com/firebase/angularfire/issues/768
 
+//test@gmail.com
+//test1234
 (function() {
 
     "use strict";
@@ -12,13 +14,20 @@
         vm.validateEmail = validateEmail;
         vm.login = login;
         vm.showToast = showToast;
+        vm.username = "";
+        vm.password = "";
             
         var config = {
-          apiKey: "AIzaSyAfh1IU93CQfo9nyJqnxxcZ0R7z3Uve3nE",
-          authDomain: "dronemap-b66a3.firebaseapp.com",
-          databaseURL: "https://dronemap-b66a3.firebaseio.com"
+            apiKey: "AIzaSyAfh1IU93CQfo9nyJqnxxcZ0R7z3Uve3nE",
+            authDomain: "dronemap-b66a3.firebaseapp.com",
+            databaseURL: "https://dronemap-b66a3.firebaseio.com",
+            projectId: "dronemap-b66a3",
+            storageBucket: "dronemap-b66a3.appspot.com",
+            messagingSenderId: "610754060845"
         };
-        firebase.initializeApp(config);
+        if (!firebase.apps.length) {
+            firebase.initializeApp(config);
+        }
 
         var rootRef = firebase.auth();
         //var firebaseREF = new Firebase('https://dronemap-883ec.firebaseio.com');
@@ -33,29 +42,22 @@
         }
         //console.log("authController");
         
-        function login(username, password) { 
-            if(vm.validateEmail(username)) {
-               firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
-                    // Handle Errors here.
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    // [START_EXCLUDE]
-                    if (errorCode === 'auth/wrong-password') {
-                        vm.showToast("Wrong password");
-                    } else {
-                        vm.showToast(errorMessage);
-                    }
-                    console.log(error);
-                });
-            } else {
-                vm.showToast("Invalid Email");
-            }
-            
-        }   
+        function login(uesrname, password){
+            vm.username = username;
+            console.log(vm.username);
+//            var em = this.username;
+//            var ps = this.password;
+//            var authentication = firebase.auth().signInWithEmailAndPassword(em, ps);
+//            console.log(authentication);
+        }
             
         function validateEmail(email) {
             var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email);
         }
+
+  
+
+   
     }]);
 })();
